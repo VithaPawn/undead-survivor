080ab7f0427939204d52677f2fcd48ee16bc178f
thanglv <thanglv@mirabo-global.com>
2024-06-13 09:04:19 +0700

    feat: add UI package

--- Assets/_Scripts/Canvases/GameOverCanvas.cs
+++ Assets/_Scripts/Canvases/GameOverCanvas.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOverCanvas : BaseCanvas {
+    #region Constants
+    private const string BEST_SCORE = "US_BestScore";
+    #endregion Constants
+
     #region Variables
     [Header("Children UIs")]
     [SerializeField] private Button homeButton;
     [SerializeField] private Button replayButton;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI bestText;
+    [SerializeField] private Image newIcon;
     [Header("Game State")]
     [SerializeField] private GameStateManagerSO gameStateManagerSO;
     [SerializeField] private GameStateSO gameMenuStateSO;
     [SerializeField] private GameStateSO gamePlayingStateSO;
     [Header("Statistics")]
-    [SerializeField] private FloatVariableSO defeatedEnemiesAmount;
     [SerializeField] private FloatVariableSO expPoints;
+    private float bestScore;
     #endregion Variables
 
     #region Methods
@@ -58,8 +63,23 @@ public class GameOverCanvas : BaseCanvas {
 
     protected override void Show()
     {
-        scoreText.text = defeatedEnemiesAmount.GetValue().ToString();
-        bestText.text = expPoints.GetValue().ToString();
+        //Get best score from local memory
+        bestScore = PlayerPrefs.GetFloat(BEST_SCORE, 0);
+        float currentScore = expPoints.GetValue();
+
+        //Update UI
+        scoreText.text = expPoints.GetValue().ToString();
+        if (bestScore < currentScore)
+        {
+            bestText.text = currentScore.ToString();
+            newIcon.gameObject.SetActive(true);
+            PlayerPrefs.SetFloat(BEST_SCORE, currentScore);
+        }
+        else
+        {
+            newIcon.gameObject.SetActive(false);
+            bestText.text = bestScore.ToString();
+        }
         base.Show();
     }
     #endregion Methods
